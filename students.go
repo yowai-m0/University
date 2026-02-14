644765ab7468ec83e593d9c2801c41cf8feaea7a
alim <aartmazov@gmail.com>
2026-02-14 23:50:57 +0300

    Fix2

--- students.go
+++ students.go
@@ -7,41 +7,41 @@ import (
 	"log"
 	"os"
 	"strings"
-    
-    "github.com/joho/godotenv"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
-    // ===== 1. ЗАГРУЖАЕМ .env ФАЙЛ =====
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Ошибка загрузки .env файла:", err)
-    }
-    
-    // ===== 2. ЧИТАЕМ ПЕРЕМЕННЫЕ =====
-    host := os.Getenv("DB_HOST")
-    port := os.Getenv("DB_PORT")
-    dbname := os.Getenv("DB_NAME")
-    user := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASSWORD")
-    
-    // ===== 3. СОБИРАЕМ СТРОКУ ПОДКЛЮЧЕНИЯ =====
-    connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
-        host, port, dbname, user, password)
-    
-    // ===== 4. ПОДКЛЮЧАЕМСЯ =====
-    conn, err := pgx.Connect(context.Background(), connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Успешное подключение к БД") 
+	reader := bufio.NewReader(os.Stdin) // читатель строк
+	// загружаем env файл
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки .env файла:", err)
+	}
 
-    fmt.Println()
+	// читаем переменные
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
 
-    fmt.Println("Создаем БД")
-    _, err = conn.Exec(context.Background(), `
+	// сбор строки подключения
+	connStr := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		host, port, dbname, user, password)
+
+	// подключение
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Успешное подключение к БД")
+
+	fmt.Println()
+
+	fmt.Println("Создаем БД")
+	_, err = conn.Exec(context.Background(), `
         CREATE TABLE IF NOT EXISTS students (
             id SERIAL PRIMARY KEY,
             name TEXT,
@@ -49,29 +49,29 @@ func main() {
             course INT
         )
     `)
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    _, err = conn.Exec(context.Background(), "DELETE FROM students")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Таблица очищена")
+	_, err = conn.Exec(context.Background(), "DELETE FROM students")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Таблица очищена")
 
-    _, err = conn.Exec(context.Background(),
-        "ALTER SEQUENCE students_id_seq RESTART WITH 1")
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Счетчик обновлен успешно")
+	_, err = conn.Exec(context.Background(),
+		"ALTER SEQUENCE students_id_seq RESTART WITH 1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Счетчик обновлен успешно")
 
-    students := []struct {
-        name string
-        age int
-        course int
-    }{
-        {"Иван Петров", 19, 2},
+	students := []struct {
+		name   string
+		age    int
+		course int
+	}{
+		{"Иван Петров", 19, 2},
 		{"Анна Смирнова", 20, 3},
 		{"Петр Иванов", 21, 4},
 		{"Алексей Иванов", 19, 2},
@@ -99,164 +99,164 @@ func main() {
 		{"Роман Чернышёв", 24, 5},
 		{"Елизавета Калинина", 19, 2},
 		{"Игорь Фролов", 25, 6},
-    }
+	}
 
-    for _, s := range students {
-        _, err = conn.Exec(context.Background(),
-            "INSERT INTO students (name, age, course) VALUES ($1, $2, $3)",
-            s.name, s.age, s.course)
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
-    fmt.Println("Успешно добавленые студенты")
+	for _, s := range students {
+		_, err = conn.Exec(context.Background(),
+			"INSERT INTO students (name, age, course) VALUES ($1, $2, $3)",
+			s.name, s.age, s.course)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Успешно добавленые студенты")
 
-    var count int
-    err = conn.QueryRow(context.Background(), 
-        "SELECT COUNT(*) FROM students").Scan(&count)
-    if err != nil {
-        log.Fatal(err)
-    } 
-    fmt.Printf("Всего записей: %d\n", count)  
+	var count int
+	err = conn.QueryRow(context.Background(),
+		"SELECT COUNT(*) FROM students").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Всего записей: %d\n", count)
 
-    for {
-        Menu()
-        fmt.Println("Выберите действие")
-        var choise int
-        fmt.Scan(&choise)
-        fmt.Scanln()
+	for {
+		Menu()
+		fmt.Println("Выберите действие")
+		var choise int
+		fmt.Scan(&choise)
+		fmt.Scanln()
 
-        switch choise {
-        case 1:
-            row, err := conn.Query(context.Background(),
-                "SELECT id, name, age, course FROM students ORDER BY id")
-            if err != nil {
-                log.Fatal(err)
-            }
-            defer row.Close()
+		switch choise {
+		case 1:
+			row, err := conn.Query(context.Background(),
+				"SELECT id, name, age, course FROM students ORDER BY id")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer row.Close()
 
-            fmt.Println("=== Список студентов ===")
-            fmt.Println(" ID  |  Name  |  Age  |  Course")
+			fmt.Println("=== Список студентов ===")
+			fmt.Println(" ID  |  Name  |  Age  |  Course")
 
-            for row.Next() {
-                var id int
-                var name string
-                var age int
-                var course int
+			for row.Next() {
+				var id int
+				var name string
+				var age int
+				var course int
 
-                err = row.Scan(&id, &name, &age, &course)
-                if err != nil {
-                    log.Fatal(err)
-                }
-                fmt.Printf(" %d  |  %s  |  %d  |  %d\n", id, name, age, course)
-            }
-            fmt.Println()
+				err = row.Scan(&id, &name, &age, &course)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf(" %d  |  %s  |  %d  |  %d\n", id, name, age, course)
+			}
+			fmt.Println()
 
-        case 2:
-            fmt.Println("Введите имя и фамилию студента")
-            var nameStud string
-            nameStud, _ = reader.ReadString('\n')
-            nameStud = strings.TrimSpace(nameStud)
+		case 2:
+			fmt.Println("Введите имя и фамилию студента")
+			var nameStud string
+			nameStud, _ = reader.ReadString('\n')
+			nameStud = strings.TrimSpace(nameStud)
 
-            fmt.Println("Введите возраст")
-            var ageStud int
-            fmt.Scan(&ageStud)
+			fmt.Println("Введите возраст")
+			var ageStud int
+			fmt.Scan(&ageStud)
 
-            fmt.Println("Введите курс обучения")
-            var courseStud int
-            fmt.Scan(&courseStud)
+			fmt.Println("Введите курс обучения")
+			var courseStud int
+			fmt.Scan(&courseStud)
 
-            _, err = conn.Exec(context.Background(),
-                "INSERT INTO students (name, age, course) VALUES ($1, $2, $3)",
-                nameStud, ageStud, courseStud)
-            if err != nil {
-                log.Fatal(err)
-            }      
-            fmt.Printf("Студент %s успешно добавлен\n", nameStud)
-            fmt.Println()
+			_, err = conn.Exec(context.Background(),
+				"INSERT INTO students (name, age, course) VALUES ($1, $2, $3)",
+				nameStud, ageStud, courseStud)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Студент %s успешно добавлен\n", nameStud)
+			fmt.Println()
 
-        case 3:
-            fmt.Println("Введите курс чтобы посмотреть студентов")
-            var courseFind int
-            fmt.Scan(&courseFind)
+		case 3:
+			fmt.Println("Введите курс чтобы посмотреть студентов")
+			var courseFind int
+			fmt.Scan(&courseFind)
 
-            rows, err := conn.Query(context.Background(),
-                "SELECT name, age FROM students WHERE course = $1", courseFind)
-            if err != nil {
-                log.Fatal(err)
-            }   
+			rows, err := conn.Query(context.Background(),
+				"SELECT name, age FROM students WHERE course = $1", courseFind)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            fmt.Printf("Студенты %d курса:\n", courseFind)
-            found := false
-            for rows.Next() {
-                var name string
-                var age int
-                err = rows.Scan(&name, &age)
-                if err != nil {
-                    log.Fatal(err)
-                }
-                fmt.Printf(" - %s (%d лет)\n", name, age)
-                found = true
-            }
+			fmt.Printf("Студенты %d курса:\n", courseFind)
+			found := false
+			for rows.Next() {
+				var name string
+				var age int
+				err = rows.Scan(&name, &age)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf(" - %s (%d лет)\n", name, age)
+				found = true
+			}
 
-            if !found {
-                fmt.Printf("Студенты на этом курсе(%d) не найдены\n", courseFind)
-            }
-            fmt.Println()
+			if !found {
+				fmt.Printf("Студенты на этом курсе(%d) не найдены\n", courseFind)
+			}
+			fmt.Println()
 
-        case 4:
-            fmt.Println("Введите ID студента, чтобы обновить его курс")
-            var idStud int
-            fmt.Scan(&idStud)
+		case 4:
+			fmt.Println("Введите ID студента, чтобы обновить его курс")
+			var idStud int
+			fmt.Scan(&idStud)
 
-            fmt.Println("Введите нужный курс(1-6)")
-            var courseNew int
-            fmt.Scan(&courseNew)
+			fmt.Println("Введите нужный курс(1-6)")
+			var courseNew int
+			fmt.Scan(&courseNew)
 
-            res, err := conn.Exec(context.Background(),
-                "UPDATE students SET course = $1 WHERE id = $2", courseNew, idStud)
-            if err != nil {
-                log.Fatal(err)
-            }
+			res, err := conn.Exec(context.Background(),
+				"UPDATE students SET course = $1 WHERE id = $2", courseNew, idStud)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            if res.RowsAffected() > 0 {
-                fmt.Println("Курс обновлен")
-            }else {
-                fmt.Println("Студент не найден")
-            }
-            fmt.Println()
+			if res.RowsAffected() > 0 {
+				fmt.Println("Курс обновлен")
+			} else {
+				fmt.Println("Студент не найден")
+			}
+			fmt.Println()
 
-        case 5:
-            fmt.Println("Введите ID студента для удаления")
-            var nowId int
-            fmt.Scan(&nowId)
+		case 5:
+			fmt.Println("Введите ID студента для удаления")
+			var nowId int
+			fmt.Scan(&nowId)
 
-            res, err := conn.Exec(context.Background(),
-                "DELETE FROM students WHERE id = $1", nowId)
-            if err != nil {
-                log.Fatal(err)
-            }
+			res, err := conn.Exec(context.Background(),
+				"DELETE FROM students WHERE id = $1", nowId)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            if res.RowsAffected() > 0 {
-                fmt.Println("Студент успешно удален")
-            }else {
-                fmt.Println("Студент не найден")
-            }
-        case 0:
-            fmt.Println("ПОКА!")
-            conn.Close(context.Background())
-            fmt.Println("Таблица закрыта успешно")
-            return
-        }
-    }
+			if res.RowsAffected() > 0 {
+				fmt.Println("Студент успешно удален")
+			} else {
+				fmt.Println("Студент не найден")
+			}
+		case 0:
+			fmt.Println("ПОКА!")
+			conn.Close(context.Background())
+			fmt.Println("Таблица закрыта успешно")
+			return
+		}
+	}
 }
 
 func Menu() {
-    fmt.Println("====== Menu ======")
-    fmt.Println("1. Показать всех студентов")
-    fmt.Println("2. Добавить студента")
-    fmt.Println("3. Найти по курсу")
-    fmt.Println("4. Обновить курс")
-    fmt.Println("5. Удалить студента")
-    fmt.Println("0. Exit")
+	fmt.Println("====== Menu ======")
+	fmt.Println("1. Показать всех студентов")
+	fmt.Println("2. Добавить студента")
+	fmt.Println("3. Найти по курсу")
+	fmt.Println("4. Обновить курс")
+	fmt.Println("5. Удалить студента")
+	fmt.Println("0. Exit")
 }
